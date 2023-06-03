ca2c8f90d07d8d2f5eff85b828aad855119fa83b
David Gasinski <dgasinski1@sheffield.ac.uk>
2023-06-03 17:26:37 +0100

    bot functionality

--- db.py
+++ db.py
@@ -1,1 +1,21 @@
 import sqlite3
+
+class DB():
+    def __init__(self, DB):
+        self.conn = sqlite3.connect(DB)
+        self.cur = self.conn.cursor()
+
+    def update_record(self, id: int, val: str):
+        self.cur.execute("UPDATE users SET wallet=? WHERE id=?", (val, id))
+        self.con.commit()
+
+    def remove_record(self, id: str):
+        self.cur.execute("DELETE FROM users WHERE id=?", id)
+
+    def add_record(self, id: int, wallet: str):
+        self.cur.execute("INSERT INTO users VALUES (?, ?)", (id, wallet))
+        self.conn.commit()
+
+    def get_record(self, id: int):
+        return self.cur.execute("SELECT * from users WHERE id=?", id)
+    
--- init.py
+++ init.py
@@ -1,12 +1,24 @@
 from dotenv import load_dotenv
-from bot import Bot
 import os
 import discord
+from discord.ext import commands
+from commands.eth import Eth
+from db import DB
+#from commands.add_wallet import Wallet
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
+DB_PATH = os.getenv('DB')
 
-if __name__ == "__main__":
-    intents = discord.Intents.default()
-    client = Bot(intents=intents)
-    client.run(TOKEN)
+if __name__ == "__main__":  
+    intent = discord.Intents.default()
+    intent.message_content = True #v2
+    bot = commands.Bot(command_prefix='!', intents=intent)
+
+
+    @bot.event
+    async def on_ready():
+         db = DB(DB_PATH)
+         await bot.add_cog(Eth(bot))
+    #bot.add_cog(Wallet(bot))
+    bot.run(TOKEN)
